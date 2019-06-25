1bd481bb6341c325fa7c6b5f79e1fb1e22821729
uckova <uckova57rus@gmail.com>
2019-06-26 02:12:16 +0300

    witch CHECK

--- app/src/main/java/com/example/logandpas/server/Server.kt
+++ app/src/main/java/com/example/logandpas/server/Server.kt
@@ -2,21 +2,27 @@ package com.example.logandpas.server
 
 class Server {
     private val data = mapOf("john@domain.tld" to 123123, "jane@domain.tld" to 12345)
+    private var check = false
 
     fun serverRun(login: String, password: Int): String {
         if (data.contains(login))
-            if (data.getValue(login) == password)
+            if (data.getValue(login) == password){
+                check = true
                 return answerOk(login)
+            }
 
         return answerError()
     }
 
     fun getProfil(token: Int): String? {
-        return if (token == 0) {
-            Profil().john()
-        } else {
-            Profil().jane()
+        if (check) {
+            return if (token == 0) {
+                Profil().john()
+            } else {
+                Profil().jane()
+            }
         }
+       return ""
     }
 
 }
